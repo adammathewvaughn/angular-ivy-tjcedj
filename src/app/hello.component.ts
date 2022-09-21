42d4e22e3153f6e24c3bee2a7fa453ff780ed177
adammathewvaughn <adammathewvaughn@gmail.com>
2022-09-20 23:09:40 -0500

    practicin tabular data presentation

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  selector: 'tabledata',
+  template: `<h2>{{name}}</h2>`,
   styles: [`h1 { font-family: Lato; }`]
 })
-export class HelloComponent  {
+export class TableDataComponent  {
   @Input() name: string;
 }
